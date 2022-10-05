9e5de96f69e07b940d816f8f49dca3027a747235
Oleksii Moroz <blackswift1992@gmail.com>
2022-10-05 05:45:53 +0300

    separate methods in MessageMenuVC and EditMessageWarningVC

--- Flash Chat iOS13/Controllers/MessageMenuViewController.swift
+++ Flash Chat iOS13/Controllers/MessageMenuViewController.swift
@@ -7,42 +7,53 @@ class MessageMenuViewController: UIViewController {
     private var editButtonPressedCallBack: (() -> ())?
     private var deleteButtonPressedCallBack: (() -> ())?
     
-    
-    func setEditButtonPressedCallBack(_ editCallBack: (() -> ())?) {
-        editButtonPressedCallBack = editCallBack
-    }
-    
-    
-    func setDeleteButtonPressedCallBack(_ deleteCallBack: (() -> ())?) {
-        deleteButtonPressedCallBack = deleteCallBack
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         customizeViewElements()
     }
+}
 
-    
-    private func customizeViewElements() {
-        deleteButton.layer.cornerRadius = 18
-        editButton.layer.cornerRadius = 18
+
+//MARK: - Public methods
+
+
+extension MessageMenuViewController {
+    func setEditButtonPressedCallBack(_ editCallBack: (() -> ())?) {
+        editButtonPressedCallBack = editCallBack
     }
     
-    
+    func setDeleteButtonPressedCallBack(_ deleteCallBack: (() -> ())?) {
+        deleteButtonPressedCallBack = deleteCallBack
+    }
+}
+
+
+//MARK: - @IBActions
+
+
+extension MessageMenuViewController {
     @IBAction private func deleteButtonPressed(_ sender: UIButton) {
         dismiss(animated: true) {
             self.deleteButtonPressedCallBack?()
         }
     }
     
-    
     @IBAction private func editButtonPressed(_ sender: UIButton) {
         dismiss(animated: false) {
             self.editButtonPressedCallBack?()
         }
     }
+}
 
+
+//MARK: - Set up methods
+
+
+extension MessageMenuViewController {
+    private func customizeViewElements() {
+        deleteButton.layer.cornerRadius = 18
+        editButton.layer.cornerRadius = 18
+    }
     
     //any touch out of the UI element heads back to previous view
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -50,5 +61,4 @@ class MessageMenuViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 }
-
 
